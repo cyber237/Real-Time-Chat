fcfbf1ac00188a8c9c46e7a22d77be66a8bcbbef
cyber237 <nintaidick67@gmail.com>
2020-07-29 20:54:38 +0100

    Added comments to consumer model

--- RealTimeChat/RealTimeChat/consumer.py
+++ RealTimeChat/RealTimeChat/consumer.py
@@ -4,12 +4,22 @@ import datetime
 from channels.layers import get_channel_layer
 
 class Client(AsyncJsonWebsocketConsumer):
+    # class Client is our consumer for this connection
+    # Tt manages the websocket connected to the server it also 
+    # carries all methods we could apply to the connection
+
     async def websocket_connect(self):
+        # the websocket connect method is called any time
+        # a connection is initiated to the server during handshake
+
         self.channel_name=self.scope['id']
         self.channel_layer=get_channel_layer()
         await self.accept()
     
     async def receive_json(self,content):
+
+        # This method is called anytime we recieve a packet from the connection
+
         command=content["command"]
         if command=="message.send.private":
             await self.send_private(content["message"])
@@ -17,6 +27,8 @@ class Client(AsyncJsonWebsocketConsumer):
             await self.send_group(content['message'])
     
     async def send_private(self,content):
+        # This method is called if the command of the packet received is a message to another user
+        # we call it "private"
         receiver=content['receiver']
         await self.channel_layer.send(
             receiver,
@@ -29,6 +41,9 @@ class Client(AsyncJsonWebsocketConsumer):
 
     
     async def send_group(self,content):
+
+        # This method is called any time the command in the packet is a group message
+        
         receiver=content['receiver']
         await self.channel_layer.group_send(
             receiver,
